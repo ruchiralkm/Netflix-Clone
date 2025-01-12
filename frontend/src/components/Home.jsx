45bd502491738afb902e9015c7a04b9b69fa5428
Ruchira Kaluarachchi <151660406+ruchiralkm@users.noreply.github.com>
2025-01-12 08:06:49 +0530

    Update nav, home

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -7,10 +7,43 @@ const Home = () => {
 
   return (
     <div>
-      <Nav />
       {/* <h1>Welcome, {state.name}!</h1>
       <p>Email: {state.email}</p> */}
+
+      {/* Pass state.name to Nav */}
+      <Nav userName={state.name} userEmail={state.email} />
       <Carousel />
+
+      <div className="flex items-center justify-center min-h-screen p-4 mt-[-200px] bg-gray-100">
+        <div className="grid grid-cols-1 gap-6 max-w-8xl sm:grid-cols-4">
+          {/* Movie Card 1 */}
+          <div className="overflow-hidden transition-transform duration-300 bg-white rounded-lg shadow-lg hover:scale-105">
+            <img
+              src="https://dnm.nflximg.net/api/v6/rkETp35xJVj-6WaffQsS77awykM/AAAABShbW-dwcF0WdzxNH6Wrw42S0VoyMM8gvcOoQ4PhLLD2F-NMiotNJBl5sQGjhKeSh0FuMFj8CFaFkL955pBwjNPpwL4qhGHc5wOcTrhQUnzOcB162aWglEX60Dlbm-VJo0YwIw.jpg"
+              alt="Movie 1"
+              className="object-cover w-full h-52"
+            />
+            <div className="p-4">
+              <h2 className="mb-2 text-xl font-bold text-gray-800">
+                Inception
+              </h2>
+              <p className="mb-4 text-gray-600">
+                A mind-bending thriller by Christopher Nolan.
+              </p>
+              <div className="flex items-center justify-between text-sm text-gray-500">
+                <span>⭐ 8.8</span>
+                <span>Genre: Sci-Fi</span>
+                <span>2010</span>
+              </div>
+              <div className="p-1 mt-2">
+                <button>click</button>
+              </div>
+            </div>
+          </div>
+
+          {/* Movie Card 2 */}
+        </div>
+      </div>
     </div>
   );
 };
--- frontend/src/components/Nav/Nav.jsx
+++ frontend/src/components/Nav/Nav.jsx
@@ -4,24 +4,23 @@ import { Bars3Icon, XMarkIcon, BellIcon } from "@heroicons/react/24/outline";
 import { Link } from "react-router-dom";
 
 const navigation = [
-  { name: "Dashboard", href: "#", current: true },
-  { name: "Team", href: "#", current: false },
-  { name: "Projects", href: "#", current: false },
-  { name: "Calendar", href: "#", current: false },
+  { name: "Dashboard", to: "/home", current: true },
+  { name: "Team", to: "/team", current: false },
+  { name: "Projects", to: "/projects", current: false },
+  { name: "Calendar", to: "/calendar", current: false },
 ];
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
-function Nav() {
+function Nav({ userName, userEmail }) {
   return (
     <Disclosure as="nav" className="bg-gray-800">
       {({ open }) => (
         <>
           <div className="px-2 mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div className="relative flex items-center justify-between h-16">
-              {/* Mobile menu button */}
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 <Disclosure.Button className="inline-flex items-center justify-center p-2 text-gray-400 rounded-md hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="sr-only">Open main menu</span>
@@ -43,9 +42,9 @@ function Nav() {
                 <div className="hidden sm:ml-6 sm:block">
                   <div className="flex space-x-4">
                     {navigation.map((item) => (
-                      <a
+                      <Link
                         key={item.name}
-                        href={item.href}
+                        to={item.to}
                         className={classNames(
                           item.current
                             ? "bg-gray-900 text-white"
@@ -55,7 +54,7 @@ function Nav() {
                         aria-current={item.current ? "page" : undefined}
                       >
                         {item.name}
-                      </a>
+                      </Link>
                     ))}
                   </div>
                 </div>
@@ -68,12 +67,14 @@ function Nav() {
                   <span className="sr-only">View notifications</span>
                   <BellIcon className="w-6 h-6" aria-hidden="true" />
                 </button>
-
-                {/* Profile dropdown */}
                 <Menu as="div" className="relative ml-3">
                   <div>
-                    <Menu.Button className="flex text-sm bg-gray-800 rounded-full focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                      <span className="sr-only">Open user menu</span>
+                    <Menu.Button className="flex text-sm bg-gray-800 rounded-full">
+                      <div className="flex flex-col items-end pr-2">
+                        <span className="p-1 text-white">
+                          Welcome, {userName}
+                        </span>
+                      </div>
                       <img
                         className="w-8 h-8 rounded-sm"
                         src="https://wallpapers.com/images/hd/netflix-profile-pictures-1000-x-1000-88wkdmjrorckekha.jpg"
@@ -93,35 +94,34 @@ function Nav() {
                     <Menu.Items className="absolute right-0 z-10 w-48 py-1 mt-2 origin-top-right bg-white rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                       <Menu.Item>
                         {({ active }) => (
-                          <a
-                            href="#"
+                          <Link
+                            to="/profile"
                             className={classNames(
                               active ? "bg-gray-100" : "",
                               "block px-4 py-2 text-sm text-gray-700"
                             )}
                           >
                             Your Profile
-                          </a>
+                          </Link>
                         )}
                       </Menu.Item>
                       <Menu.Item>
                         {({ active }) => (
-                          <a
-                            href="#"
+                          <Link
+                            to="/settings"
                             className={classNames(
                               active ? "bg-gray-100" : "",
                               "block px-4 py-2 text-sm text-gray-700"
                             )}
                           >
                             Settings
-                          </a>
+                          </Link>
                         )}
                       </Menu.Item>
                       <Menu.Item>
                         {({ active }) => (
                           <Link
                             to="/login"
-                            href="#"
                             className={classNames(
                               active ? "bg-gray-100" : "",
                               "block px-4 py-2 text-sm text-gray-700"
@@ -137,14 +137,13 @@ function Nav() {
               </div>
             </div>
           </div>
-
           <Disclosure.Panel className="sm:hidden">
             <div className="px-2 pt-2 pb-3 space-y-1">
               {navigation.map((item) => (
                 <Disclosure.Button
                   key={item.name}
-                  as="a"
-                  href={item.href}
+                  as={Link}
+                  to={item.to}
                   className={classNames(
                     item.current
                       ? "bg-gray-900 text-white"
